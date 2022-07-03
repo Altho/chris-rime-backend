dc8fc7c415d10cebe59b4c39e355eb4a394a2044
Altho <alan.at.thomas@gmail.com>
2022-07-03 09:18:14 +0200

    changed blig id to slug

--- src/api/blog/content-types/blog/lifecycles.js
+++ src/api/blog/content-types/blog/lifecycles.js
@@ -6,13 +6,13 @@ module.exports = {
   async beforeCreate(event) {
     const { data } = event.params;
     if (data.title) {
-      data.slug = slugify(data.name, { lower: true });
+      data.slug = slugify(data.title, { lower: true });
     }
   },
   async beforeUpdate(event) {
     const { data } = event.params;
     if (data.title) {
-      data.slug = slugify(data.name, { lower: true });
+      data.slug = slugify(data.title, { lower: true });
     }
   },
 };
